1a6077e7d26e0f132eb9d16ad10286d506020b90
Bartłomiej Kłos <bartlomiej.klos03@gmail.com>
2026-07-26 12:22:24 +0200

    Setup React, Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-stone-100">
+      <h1 className="text-4xl font-semibold text-green-900">
+        GiardDesign
+      </h1>
+    </main>
+  );
+}
+
+export default App;
